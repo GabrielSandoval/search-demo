ab48f42cb76bae6e05c1dc3c23414ca3aaf477b9
Gabriel Sandoval <gabrielsandoval1020@gmail.com>
2023-07-25 17:51:24 +0800

    Add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+counter = 0
+File.foreach("db/seeds/News_Category_Dataset_v3.json") do |line|
+  article = JSON.parse(line)
+  Article.create(title: article["headline"])
+  counter += 1
+  puts "Articles created #{counter} / 10000" if counter % 1000 == 0
+end
