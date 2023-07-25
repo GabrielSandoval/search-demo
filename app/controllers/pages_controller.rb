cbd9febd2f90fd6f82b77daa4b68feda8f5daf62
Gabriel Sandoval <gabrielsandoval1020@gmail.com>
2023-07-25 18:00:23 +0800

    Use ArticlesIndex for search

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,11 @@
 class PagesController < ApplicationController
   def index
+    @query = params[:query]
+    @results = ArticlesIndex.query(
+      query_string: {
+        fields: [:title],
+        query: @query
+      }
+    ).as_json if @query.present?
   end
 end
